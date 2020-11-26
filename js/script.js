bca61ff580910f9ad906c72398bacb0722f9fd8c
Kirill <mrbomberman99@gmail.com>
2020-11-26 16:12:57 +0300

    second commit of project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 const numberOfFilms = +prompt('How many films have you already seen?', '');
 
 
@@ -9,13 +10,32 @@ const personalMovieDB = {
     privat : false
 };
 
-const question1 = prompt('One of the last films you have seen?', '');
-const star1 = prompt('How many stars you will give him?', '');
-const question2 = prompt('One of the last films you have seen?', '');
-const star2 = prompt('How many stars you will give him?', '');
 
-personalMovieDB.movies[question1] = star1;
-personalMovieDB.movies[question2] = star2;
 
+for (let i = 0; i < 2; i++) {
+    const question = prompt('One of the last films you have seen?', ''),
+        rating = prompt('How many stars you will give him?', '');
+
+    if (question != null && rating != null && question != '' && rating != '' && question.length < 50 && rating.length < 50) {
+        personalMovieDB.movies[question] = rating;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+
+}
+
+
+
+if (personalMovieDB.count < 10) {
+    console.log('Oh, you have seen few films');
+} else if (10 <= personalMovieDB.count < 30) {
+    console.log('Oh, you have seen not so many films');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are a really great watcher of films');
+} else {
+    console.log('Oh, It is a mistake');
+}
 
 console.log(personalMovieDB);
